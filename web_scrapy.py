f979bb14df85b9586af0e89f8e7ff8736a149655
DragonyteEvol <81652671+DragonyteEvol@users.noreply.github.com>
2021-05-27 23:08:56 -0500

    Delete web_scrapy.py

--- web_scrapy.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-r=requests.get("https://www.pokemon.com/el/")
-soup = BeautifulSoup(r.content,"lxml")
-tags= soup("a")
-for tag in tags:
-    print(tag.get('href'))
