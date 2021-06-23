994a187e8af614bd8abe76721ebc56773bcf55db
Dragonyte Evol <AcuarionEvol@outlook.com>
2021-06-23 18:34:03 -0500

    tag url value

--- web_scrapy.py
+++ web_scrapy.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 url=input("url:")
+tag=input("tag:")
+value=input("value:")
 r=requests.get(url)
 soup = BeautifulSoup(r.content,"lxml")
-tags= soup("a")
-for tag in tags:
-    print(tag.get('href'))
+tags= soup(tag)
+for tagg in tags:
+    print(tagg.get(value))
